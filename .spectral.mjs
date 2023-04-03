d9d004f324888269aa0a20db036b9d8f3df27ecd
James Clague <james.clague@brct.io>
2023-04-02 23:28:22 -0600

    Initial commit

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE5MzY0Ng/spectral.js?branch_slug=master&token=1c3889a7-2541-4cd2-97d1-113296f3a90c";
+export default { extends: ruleset };
